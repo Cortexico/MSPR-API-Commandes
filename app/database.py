f04898d6ef43c56bab308eb5e6870b213548f680
laghrish <hatim.laghrissi@gmail.com>
2024-10-27 22:57:44 +0100

    modif workflow ET databse.py

--- app/database.py
+++ app/database.py
@@ -1,3 +1,33 @@
+# import os
+# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+# from dotenv import load_dotenv
+
+# # Charger les variables d'environnement
+# load_dotenv()
+
+# POSTGRES_USER = os.getenv("POSTGRES_USER")
+# POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+# POSTGRES_DB = os.getenv("POSTGRES_DB")
+# POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+# POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+
+# DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+# engine = create_async_engine(DATABASE_URL, echo=True)
+
+# async_session = sessionmaker(
+#     bind=engine, expire_on_commit=False, class_=AsyncSession
+# )
+
+# Base = declarative_base()
+
+# # Fonction pour créer les tables au démarrage
+# async def create_tables():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
+
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
@@ -13,6 +43,15 @@ POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
+# Assurez-vous que le port est un entier valide
+if POSTGRES_PORT:
+    try:
+        # Convertit d'abord en float puis en int pour gérer les ports spécifiés comme flottants
+        POSTGRES_PORT = int(float(POSTGRES_PORT))
+    except ValueError:
+        print("Le port fourni est invalide, utilisation du port par défaut 5432.")
+        POSTGRES_PORT = 5432  # Utilisez un port par défaut ou gérez autrement
+
 DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
@@ -27,3 +66,4 @@ Base = declarative_base()
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
